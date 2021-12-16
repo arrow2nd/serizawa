5994ce255cab96c018b4fe9c2bef6c40961b11a1
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-12-16 20:54:41 +0900

    fix: ディレクトリパスの表示

--- src/components/config/selectDir.tsx
+++ src/components/config/selectDir.tsx
@@ -8,21 +8,14 @@ type Props = {
 const SelectDir = ({ onClick }: Props): JSX.Element => {
   const [picDir, setPicDir] = useState('')
 
-  const setTruncateDir = (dir: string) => {
-    if (dir.length > 35) {
-      dir = dir.slice(0, 35) + '...'
-    }
-    setPicDir(dir)
-  }
-
   // 初回のみ取得
   useEffect(() => {
-    window.api.getPicDir().then((dir) => setTruncateDir(dir))
+    window.api.getPicDir().then((dir) => setPicDir(dir))
   }, [])
 
   const handleClick = async () => {
     onClick()
-    setTruncateDir(await window.api.getPicDir())
+    setPicDir(await window.api.getPicDir())
   }
 
   return (
@@ -31,7 +24,7 @@ const SelectDir = ({ onClick }: Props): JSX.Element => {
       <div
         className={`flex items-center justify-between w-full mt-2 px-4 py-1 text-center text-black border rounded-2xl shadow-md`}
       >
-        <span>{picDir}</span>
+        <span className="truncate">{picDir}</span>
         <button onClick={handleClick}>
           <AiOutlineFolderOpen />
         </button>
