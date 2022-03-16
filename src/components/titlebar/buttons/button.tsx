28eda936a2fd23bea1b69366d07ce4b0d565a462
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-16 17:09:13 +0900

    refactor: ボタンクリック時のコールバック

--- src/components/titlebar/buttons/button.tsx
+++ src/components/titlebar/buttons/button.tsx
@@ -6,13 +6,21 @@ export type Button = {
   onClick: () => void
 }
 
-const UIButton = (props: Button): JSX.Element => (
-  <button
-    className="inline-flex px-3 p-6 border-0 rounded-none bg-transparent drag-none focus:outline-none hover:bg-shiny-dark transition-colors"
-    {...props}
-  >
-    {props.children}
-  </button>
-)
+const UIButton = ({ title, children, onClick }: Button): JSX.Element => {
+  const handleClick = () => {
+    onClick()
+    window.api.focusView()
+  }
+
+  return (
+    <button
+      className="inline-flex px-3 p-6 border-0 rounded-none bg-transparent drag-none focus:outline-none hover:bg-shiny-dark transition-colors"
+      title={title}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  )
+}
 
 export default UIButton
